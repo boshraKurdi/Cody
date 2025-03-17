1dbdac9fbfc1d5f94baec81109135c45e7fa4799
Lenda <welolenda@gmail.com>
2025-03-17 04:37:46 +0200

    تعليق

--- flutter/cody/lib/features/start app/presintation/views_blocs/buttom navigation bloc/buttom_navigation_bar_event.dart
+++
@@ -1,6 +0,0 @@
-part of 'buttom_navigation_bar_bloc.dart';
-
-class ButtomNavigationBarEvent {
-  final int pageIndex ;
-  ButtomNavigationBarEvent({required this.pageIndex});
-}
--- flutter/cody/lib/features/start app/presintation/views_blocs/buttom navigation bloc/buttom_navigation_bar_state.dart
+++
@@ -1,8 +0,0 @@
-part of 'buttom_navigation_bar_bloc.dart';
-
-
- class  ButtomNavigationBarState {
-  final int pageIndex;
-
-  ButtomNavigationBarState(this.pageIndex);
- }
